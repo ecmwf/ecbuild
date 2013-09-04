3cd4f2280cbf538a65603450eded2e26320f66bc
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-09-04 11:05:19 +0100

    find netcdf c++ wrapper

--- cmake/FindNetCDF.cmake
+++ cmake/FindNetCDF.cmake
@@ -6,15 +6,18 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-# - Try to find NetCDF
+# Try to find NetCDF
+# Input:
+#  * NETCDF_PATH - user defined path where to search for the library first
+#  * NETCDF_CXX  - if to search also for netcdf_c++ wrapper library
 # Once done this will define
 #  NETCDF_FOUND - System has NetCDF
 #  NETCDF_INCLUDE_DIRS - The NetCDF include directories
 #  NETCDF_LIBRARIES - The libraries needed to use NetCDF
 
 if( DEFINED NETCDF_PATH )
-	find_path(NETCDF_INCLUDE_DIR netcdf.h PATHS ${NETCDF_PATH}/include PATH_SUFFIXES netcdf NO_DEFAULT_PATH)
-	find_library(NETCDF_LIBRARY  netcdf   PATHS ${NETCDF_PATH}/lib     PATH_SUFFIXES netcdf NO_DEFAULT_PATH)
+    find_path(NETCDF_INCLUDE_DIR netcdf.h       PATHS ${NETCDF_PATH}/include PATH_SUFFIXES netcdf NO_DEFAULT_PATH)
+    find_library(NETCDF_LIBRARY  netcdf         PATHS ${NETCDF_PATH}/lib     PATH_SUFFIXES netcdf NO_DEFAULT_PATH)
 endif()
 
 find_path(NETCDF_INCLUDE_DIR netcdf.h PATH_SUFFIXES netcdf )
@@ -25,9 +28,19 @@ set( NETCDF_INCLUDE_DIRS ${NETCDF_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
 
-# handle the QUIETLY and REQUIRED arguments and set NETCDF_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(NETCDF  DEFAULT_MSG
-								  NETCDF_LIBRARY NETCDF_INCLUDE_DIR)
+if( NETCDF_CXX )
+
+    find_library( NETCDF_CXX_LIBRARY  netcdf_c++ PATHS ${NETCDF_PATH}/lib     PATH_SUFFIXES netcdf NO_DEFAULT_PATH)
+    find_library( NETCDF_CXX_LIBRARY  netcdf_c++ PATH_SUFFIXES netcdf )
+
+    find_package_handle_standard_args( NETCDF  DEFAULT_MSG NETCDF_LIBRARY NETCDF_INCLUDE_DIR)
+
+    mark_as_advanced(NETCDF_INCLUDE_DIR NETCDF_CXX_LIBRARY )
+
+else()
+
+    find_package_handle_standard_args( NETCDF  DEFAULT_MSG NETCDF_LIBRARY NETCDF_INCLUDE_DIR)
+
+endif()
 
 mark_as_advanced(NETCDF_INCLUDE_DIR NETCDF_LIBRARY )
