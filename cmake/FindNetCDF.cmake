8163903b782825590ad7a667dcb294681f7a7b14
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-09-04 18:00:18 +0100

    fix netcdf cxx wrapper find

--- cmake/FindNetCDF.cmake
+++ cmake/FindNetCDF.cmake
@@ -7,17 +7,19 @@
 # does it submit to any jurisdiction.
 
 # Try to find NetCDF
+#
 # Input:
 #  * NETCDF_PATH - user defined path where to search for the library first
 #  * NETCDF_CXX  - if to search also for netcdf_c++ wrapper library
-# Once done this will define
+#
+# Output:
 #  NETCDF_FOUND - System has NetCDF
 #  NETCDF_INCLUDE_DIRS - The NetCDF include directories
 #  NETCDF_LIBRARIES - The libraries needed to use NetCDF
 
 if( DEFINED NETCDF_PATH )
-    find_path(NETCDF_INCLUDE_DIR netcdf.h       PATHS ${NETCDF_PATH}/include PATH_SUFFIXES netcdf NO_DEFAULT_PATH)
-    find_library(NETCDF_LIBRARY  netcdf         PATHS ${NETCDF_PATH}/lib     PATH_SUFFIXES netcdf NO_DEFAULT_PATH)
+    find_path(NETCDF_INCLUDE_DIR netcdf.h       PATHS ${NETCDF_PATH}/include PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH)
+    find_library(NETCDF_LIBRARY  netcdf         PATHS ${NETCDF_PATH}/lib     PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH)
 endif()
 
 set( _ncdf_sfx   netcdf netcdf4 )
@@ -35,12 +37,16 @@ if( NETCDF_CXX )
 
     set( _ncdf_cxx netcdf_c++ netcdf_c++ )
 
+    find_path( NETCDF_CXX_INCLUDE_DIR netcdfcpp.h PATHS ${NETCDF_PATH}/include PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH)
+    find_path( NETCDF_CXX_INCLUDE_DIR netcdfcpp.h PATH_SUFFIXES ${_ncdf_sfx} )
+
     find_library( NETCDF_CXX_LIBRARY NAMES ${_ncdf_cxx} PATHS ${NETCDF_PATH}/lib PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH )
     find_library( NETCDF_CXX_LIBRARY NAMES ${_ncdf_cxx} netcdf_c++4 PATH_SUFFIXES ${_ncdf_sfx} )
 
+    list( APPEND NETCDF_INCLUDE_DIRS ${NETCDF_CXX_INCLUDE_DIR} )
     list( APPEND NETCDF_LIBRARIES    ${NETCDF_CXX_LIBRARY} )
 
-    find_package_handle_standard_args( NETCDF  DEFAULT_MSG NETCDF_LIBRARY NETCDF_CXX_LIBRARY NETCDF_INCLUDE_DIR)
+    find_package_handle_standard_args( NETCDF  DEFAULT_MSG NETCDF_LIBRARY NETCDF_CXX_LIBRARY NETCDF_INCLUDE_DIR NETCDF_CXX_INCLUDE_DIR )
 
     mark_as_advanced(NETCDF_INCLUDE_DIR NETCDF_CXX_LIBRARY )
 
