029273dec0133487ffb1ba1732f4af813719c522
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-09-05 14:25:40 +0100

    find netcdf at ecmwf automagically

--- cmake/FindNetCDF.cmake
+++ cmake/FindNetCDF.cmake
@@ -21,36 +21,38 @@
 ### TODO: generalize this into a macro for all ecbuild
 
 if( DEFINED NETCDF_PATH )
-	list( APPEND _netcdf_hints ${NETCDF_PATH} )
+    list( APPEND _netcdf_incs ${NETCDF_PATH} ${NETCDF_PATH}/include )
+    list( APPEND _netcdf_libs ${NETCDF_PATH} ${NETCDF_PATH}/lib )
 endif()
 	
-set( _base_hints /usr/local/apps/netcfd /usr/local/apps/netcfd4  )
+set( _base_hints /usr/local/apps/netcdf /usr/local/apps/netcdf4  )
 foreach( _h ${_base_hints} )
-	
+
 	if( EXISTS ${_h} )
 	
-		list( APPEND _netcdf_hints ${_h} ${_h}/current ${_h}/new ${_h}/stable )
+        list( APPEND _netcdf_incs  ${_h}/include ${_h}/current/include ${_h}/new/include ${_h}/stable/include )
+        list( APPEND _netcdf_libs  ${_h}/lib ${_h}/current/lib ${_h}/new/lib ${_h}/stable/lib )
 
 		file(GLOB _hd ${_h}/*)
-		if( IS_DIRECTORY ${_hd} )
-			list( APPEND _netcdf_hints ${_hd} )
-		endif()
-	endif()
+        foreach( d ${_hd} )
+            if( IS_DIRECTORY ${d} )
+                list( APPEND _netcdf_incs ${d}/include ${d}/LP64/include )
+                list( APPEND _netcdf_libs ${d}/lib ${d}/LP64/lib )
+            endif()
+        endforeach()
+    endif()
 
 endforeach() 
 
-debug_var( _netcdf_hints )
-
 ###
 
-set( _ncdf_isfx include include/netcdf include/netcdf4 LP64/include )
-set( _ncdf_lsfx lib lib/netcdf lib/netcdf4 LP64/lib )
+set( _ncdf_sfx netcdf netcdf4 LP64 )
 
-find_path( NETCDF_INCLUDE_DIR  netcdf.h  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_isfx} NO_DEFAULT_PATH )
-find_path( NETCDF_INCLUDE_DIR  netcdf.h  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_isfx} )
+find_path( NETCDF_INCLUDE_DIR  netcdf.h  PATHS ${_netcdf_incs} PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH )
+find_path( NETCDF_INCLUDE_DIR  netcdf.h  PATHS ${_netcdf_incs} PATH_SUFFIXES ${_ncdf_sfx} )
 
-find_library( NETCDF_LIBRARY  netcdf  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_lfx}  NO_DEFAULT_PATH )
-find_library( NETCDF_LIBRARY  netcdf  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_lfx}  )
+find_library( NETCDF_LIBRARY  netcdf  PATHS ${_netcdf_libs} PATH_SUFFIXES ${_ncdf_sfx}  NO_DEFAULT_PATH )
+find_library( NETCDF_LIBRARY  netcdf  PATHS ${_netcdf_libs} PATH_SUFFIXES ${_ncdf_sfx}  )
 
 set( NETCDF_LIBRARIES    ${NETCDF_LIBRARY} )
 set( NETCDF_INCLUDE_DIRS ${NETCDF_INCLUDE_DIR} )
@@ -59,13 +61,13 @@ include(FindPackageHandleStandardArgs)
 
 if( NETCDF_CXX )
 
-    set( _ncdf_cxx netcdf_c++ netcdf_c++ )
+    find_path( NETCDF_CXX_INCLUDE_DIR netcdfcpp.h PATHS ${_netcdf_incs} PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH)
+    find_path( NETCDF_CXX_INCLUDE_DIR netcdfcpp.h PATHS ${_netcdf_incs} PATH_SUFFIXES ${_ncdf_sfx} )
 
-    find_path( NETCDF_CXX_INCLUDE_DIR netcdfcpp.h PATHS ${NETCDF_PATH}/include PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH)
-    find_path( NETCDF_CXX_INCLUDE_DIR netcdfcpp.h PATH_SUFFIXES ${_ncdf_sfx} )
+    set( _ncdf_cxx netcdf_c++ netcdf_c++ netcdf_c++4 )
 
-    find_library( NETCDF_CXX_LIBRARY NAMES ${_ncdf_cxx} PATHS ${NETCDF_PATH}/lib PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH )
-    find_library( NETCDF_CXX_LIBRARY NAMES ${_ncdf_cxx} netcdf_c++4 PATH_SUFFIXES ${_ncdf_sfx} )
+    find_library( NETCDF_CXX_LIBRARY NAMES ${_ncdf_cxx} PATHS ${_netcdf_libs} PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH )
+    find_library( NETCDF_CXX_LIBRARY NAMES ${_ncdf_cxx} PATHS ${_netcdf_libs} PATH_SUFFIXES ${_ncdf_sfx} )
 
     list( APPEND NETCDF_INCLUDE_DIRS ${NETCDF_CXX_INCLUDE_DIR} )
     list( APPEND NETCDF_LIBRARIES    ${NETCDF_CXX_LIBRARY} )
