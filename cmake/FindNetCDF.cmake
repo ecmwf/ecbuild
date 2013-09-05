46f281c6331433c4a3e681ff1fb4e209733dd6a4
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-09-05 11:38:04 +0100

    better finding of netcdf at ecmwf

--- cmake/FindNetCDF.cmake
+++ cmake/FindNetCDF.cmake
@@ -17,16 +17,40 @@
 #  NETCDF_INCLUDE_DIRS - The NetCDF include directories
 #  NETCDF_LIBRARIES - The libraries needed to use NetCDF
 
+
+### TODO: generalize this into a macro for all ecbuild
+
 if( DEFINED NETCDF_PATH )
-    find_path(NETCDF_INCLUDE_DIR netcdf.h       PATHS ${NETCDF_PATH}/include PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH)
-    find_library(NETCDF_LIBRARY  netcdf         PATHS ${NETCDF_PATH}/lib     PATH_SUFFIXES ${_ncdf_sfx} NO_DEFAULT_PATH)
+	list( APPEND _netcdf_hints ${NETCDF_PATH} )
 endif()
+	
+set( _base_hints /usr/local/apps/netcfd /usr/local/apps/netcfd4  )
+foreach( _h ${_base_hints} )
+	
+	if( EXISTS ${_h} )
+	
+		list( APPEND _netcdf_hints ${_h} ${_h}/current ${_h}/new ${_h}/stable )
 
-set( _ncdf_sfx   netcdf netcdf4 )
+		file(GLOB _hd ${_h}/*)
+		if( IS_DIRECTORY ${_hd} )
+			list( APPEND _netcdf_hints ${_hd} )
+		endif()
+	endif()
 
-find_path( NETCDF_INCLUDE_DIR netcdf.h PATH_SUFFIXES ${_ncdf_sfx} )
+endforeach() 
 
-find_library( NETCDF_LIBRARY netcdf   PATH_SUFFIXES ${_ncdf_sfx} )
+debug_var( _netcdf_hints )
+
+###
+
+set( _ncdf_isfx include include/netcdf include/netcdf4 LP64/include )
+set( _ncdf_lsfx lib lib/netcdf lib/netcdf4 LP64/lib )
+
+find_path( NETCDF_INCLUDE_DIR  netcdf.h  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_isfx} NO_DEFAULT_PATH )
+find_path( NETCDF_INCLUDE_DIR  netcdf.h  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_isfx} )
+
+find_library( NETCDF_LIBRARY  netcdf  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_lfx}  NO_DEFAULT_PATH )
+find_library( NETCDF_LIBRARY  netcdf  HINTS ${_netcdf_hints} PATH_SUFFIXES ${_ncdf_lfx}  )
 
 set( NETCDF_LIBRARIES    ${NETCDF_LIBRARY} )
 set( NETCDF_INCLUDE_DIRS ${NETCDF_INCLUDE_DIR} )
