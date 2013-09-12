4a190451cc9fe85fa6c4dc1ba24cc096719235b0
Baudouin Raoult <mab@ecmwf.int>
2013-09-12 07:47:27 +0100

    work on pango

--- cmake/FindPango.cmake
+++ cmake/FindPango.cmake
@@ -8,7 +8,6 @@
 
 # - Try to find Pango
 
-include(FindPackageHandleStandardArgs)
 
 set(PANGO_VERSION 1.0)
 
@@ -16,8 +15,9 @@ set(PANGO_VERSION 1.0)
 find_path(PANGO_INCLUDE_DIR NAMES pangocairo.h HINTS /usr/include/pango-${PANGO_VERSION}/pango)
 find_library(PANGO_LIBRARY NAMES libpango-${PANGO_VERSION}.so)
 
+include(FindPackageHandleStandardArgs)
+
 find_package_handle_standard_args(PANGO  DEFAULT_MSG
 	PANGO_LIBRARY PANGO_INCLUDE_DIR)
-
 
 mark_as_advanced(PANGO_INCLUDE_DIR PANGO_LIBRARY)
