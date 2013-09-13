d850ab2ec407c06071274847939cd083e187bb0e
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-09-13 09:02:48 +0100

    try fix for PGI path in Magics

--- cmake/FindPGIFortran.cmake
+++ cmake/FindPGIFortran.cmake
@@ -12,15 +12,19 @@
 # list( APPEND pg_libs pgmp pgbind numa pgf90 pgf90_rpm1 pgf902 pgf90rtl  pgftnrtl nspgc pgc rt pgsse1 pgsse2 ) # init
 # list( APPEND pg_libs pgf90 pgf90_rpm1 pgf902 pgf90rtl pgftnrtl pghpf pgc pgf90 rt pgsse1 pgsse2 )             # mars client linux.2
 # list( APPEND pg_libs pgftnrtl nspgc pgc rt pgsse1 pgsse2 )                                                    # mars client linux.3
+
 list( APPEND pg_libs pgmp pgbind numa pgf90 pgf90_rpm1 pgf902 pgf90rtl pgftnrtl pghpf nspgc pgc pgf90 pgf902 pghpf_rpm1 pghpf2 rt pgsse1 pgsse2 ) # better ?                                                    #
+
 foreach( pglib ${pg_libs} )
+
     if( DEFINED PGI_PATH )
-      find_library( ${pglib}_lib  ${pglib} 
-		  HINTS /usr/local/apps/pgi/pgi-10.8/linux86-64/10.8/lib
-		  PATH ${PGI_PATH}/lib NO_DEFAULT_PATH )
+      find_library( ${pglib}_lib  ${pglib} PATH ${PGI_PATH} PATH_SUFFIXES lib libso NO_DEFAULT_PATH )
     endif()
-	find_library( ${pglib}_lib  ${pglib} HINTS /usr/local/apps/pgi/pgi-10.8/linux86-64/10.8/lib)
+
+    find_library( ${pglib}_lib  ${pglib} HINTS /usr/local/apps/pgi/pgi-10.8/linux86-64/10.8 PATH PATH_SUFFIXES lib libso )
+
     if( ${pglib}_lib )
         list( APPEND PGIFORTRAN_LIBRARIES ${${pglib}_lib} )
     endif()
+
 endforeach()
