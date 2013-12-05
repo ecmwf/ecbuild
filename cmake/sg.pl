b6de346eee7037516c1a3a94cf19e68287ae8f84
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2013-12-05 17:20:59 +0000

    Add std:: namespace to sg.pl

--- cmake/sg.pl
+++ cmake/sg.pl
@@ -84,10 +84,10 @@ foreach my $c ( @c )
 
 	if(@tmpl)
 	{
-		$spec_type = "string";
+		$spec_type = "std::string";
 		my $x = join("+ ',' + ",  map { "Traits<$_>::name()"; } @tmpl);
 		$spec = <<"EOS";
-		string("$n<\") + $x + ">"
+        std::string("$n<\") + $x + ">"
 EOS
 		$spec =~ s/\n/ /g;
 	}
