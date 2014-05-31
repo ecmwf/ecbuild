699c6b300d7b07b355771a256e37a70015856a3f
Tiago Quintino <tiago.quintino@ecmwf.int>
2014-05-31 12:33:30 +0100

    improved search for boost

--- cmake/ecbuild_setup_test_framework.cmake
+++ cmake/ecbuild_setup_test_framework.cmake
@@ -5,27 +5,32 @@ ecbuild_add_option( FEATURE TESTS
 if( ENABLE_TESTS )
 
 	# Try to find compiled boost
-	# If this is enable then can not find boost libs using env BOOST_ROOT ?
-	ecbuild_add_extra_search_paths( boost )
 
-	set(Boost_USE_MULTITHREADED      ON )
-	#set(Boost_DEBUG                 ON)
+	if( BOOST_ROOT OR BOOSTROOT OR DEFINED ENV{BOOST_ROOT} OR DEFINED ENV{BOOSTROOT} )
+		set( CMAKE_PREFIX_PATH ${BOOST_ROOT} ${BOOSTROOT} $ENV{BOOST_ROOT} $ENV{BOOSTROOT} ${CMAKE_PREFIX_PATH} )
+	endif()
 
-	find_package( Boost 1.47.0 COMPONENTS unit_test_framework test_exec_monitor  )
+	ecbuild_add_extra_search_paths( boost ) # also respects BOOST_ROOT
+
+	set( Boost_USE_MULTITHREADED  ON )
+#   set( Boost_DEBUG              ON )
+
+	find_package( Boost 1.47.0 COMPONENTS unit_test_framework test_exec_monitor )
 
 	if( Boost_FOUND AND Boost_UNIT_TEST_FRAMEWORK_LIBRARY AND Boost_TEST_EXEC_MONITOR_LIBRARY )
 
 		set( EC_BOOST_UNIT_TEST_FRAMEWORK_COMPILED 1 )
-		# message( STATUS "using compiled Boost unit test framework [${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}]" )
+
+		message( STATUS "Boost unit test framework -- FOUND [${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}]" )
 
 	else()
 
 		set( EC_BOOST_UNIT_TEST_FRAMEWORK_HEADER_ONLY 1 )
 		set( ECBUILD_BOOST_HEADER_DIR "${CMAKE_CURRENT_LIST_DIR}/contrib/boost-1.55/include" )
 
-		message( WARNING "Boost unit test framework not found, deactivating tests -- ENABLE_TESTS = OFF" )
+		message( WARNING "Boost unit test framework -- NOT FOUND : deactivating tests, ENABLE_TESTS = OFF" )
 
-		set( ENABLE_TESTS OFF )
+		set( ENABLE_TESTS OFF ) # comment out this when ecbuild paks boost unit test inside...
 
 	endif()
 
