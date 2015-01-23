bb24b97f15dfecb80e5936e616a3bec04e8140a1
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-01-23 11:03:19 +0000

    NOREMOTE skips git remote operations

--- cmake/ecbuild_bundle.cmake
+++ cmake/ecbuild_bundle.cmake
@@ -8,6 +8,8 @@
 #  - BRANCH (optional): Branch to clone
 #  - TAG (optional): Tag or commit-id to checkout 
 #  - UPDATE (optional) : Option to try to update every cmake run
+#  - NOREMOTE (optional) : Option to avoid remote operations that require network 
+#                          changes to tags that havent been fetched might fail
 
 macro( debug_here VAR )
   message( STATUS " >>>>> ${VAR} [${${VAR}}]")
@@ -37,13 +39,17 @@ endif()
 
 macro( ecbuild_git )
 
-  set( options UPDATE )
+  set( options UPDATE NOREMOTE )
   set( single_value_args PROJECT DIR URL TAG BRANCH )
   set( multi_value_args )
   cmake_parse_arguments( _PAR "${options}" "${single_value_args}" "${multi_value_args}" ${_FIRST_ARG} ${ARGN} )
 
   if( DEFINED _PAR_BRANCH AND DEFINED _PAR_TAG )
     message( FATAL_ERROR "Cannot defined both BRANCH and TAG in macro ecbuild_git" )
+  endif()
+
+  if( _PAR_UPDATE AND _PAR_NOREMOTE )
+    message( FATAL_ERROR "Cannot pass both NOREMOTE and UPDATE in macro ecbuild_git" )
   endif()
 
   if(_PAR_UNPARSED_ARGUMENTS)
@@ -130,7 +136,7 @@ macro( ecbuild_git )
       set( _needs_switch 1 )
     endif()
 
-	if( DEFINED _PAR_BRANCH AND _PAR_UPDATE )
+	if( DEFINED _PAR_BRANCH AND _PAR_UPDATE AND NOT _PAR_NOREMOTE )
 
 	  add_custom_target( git_update_${_PAR_PROJECT}
 						 COMMAND "${GIT_EXECUTABLE}" pull -q
@@ -145,14 +151,14 @@ macro( ecbuild_git )
 
     if( _needs_switch AND IS_DIRECTORY "${_PAR_DIR}/.git" )
 
-      debug_here( ABS_PAR_DIR )
-      debug_here( _sha1 )
-      debug_here( _current_branch )
-      debug_here( _current_tag )
-      debug_here( _PAR_TAG )
-      debug_here( _PAR_BRANCH )
-      debug_here( _needs_switch )
-      debug_here( _PAR_UPDATE )
+      # debug_here( ABS_PAR_DIR )
+      # debug_here( _sha1 )
+      # debug_here( _current_branch )
+      # debug_here( _current_tag )
+      # debug_here( _PAR_TAG )
+      # debug_here( _PAR_BRANCH )
+      # debug_here( _needs_switch )
+      # debug_here( _PAR_UPDATE )
 
       if( DEFINED _PAR_BRANCH )
         set ( _gitref ${_PAR_BRANCH} )
@@ -164,20 +170,26 @@ macro( ecbuild_git )
 
       # fetching latest tags and branches
 
-      message(STATUS "git fetch --all @ ${ABS_PAR_DIR}")
-      execute_process(COMMAND "${GIT_EXECUTABLE}" fetch --all -q
-        RESULT_VARIABLE nok ERROR_VARIABLE error
-        WORKING_DIRECTORY "${ABS_PAR_DIR}")
-      if(nok)
-        message(STATUS "git fetch --all in ${_PAR_DIR} failed:\n ${error}")
-      endif()
+      if( NOT _PAR_NOREMOTE )
 
-      message(STATUS "git fetch --all --tags @ ${ABS_PAR_DIR}")
-      execute_process(COMMAND "${GIT_EXECUTABLE}" fetch --all --tags -q
-        RESULT_VARIABLE nok ERROR_VARIABLE error
-        WORKING_DIRECTORY "${ABS_PAR_DIR}")
-      if(nok)
-        message(STATUS "git fetch --all --tags in ${_PAR_DIR} failed:\n ${error}")
+          message(STATUS "git fetch --all @ ${ABS_PAR_DIR}")
+          execute_process(COMMAND "${GIT_EXECUTABLE}" fetch --all -q
+            RESULT_VARIABLE nok ERROR_VARIABLE error
+            WORKING_DIRECTORY "${ABS_PAR_DIR}")
+          if(nok)
+            message(STATUS "git fetch --all in ${_PAR_DIR} failed:\n ${error}")
+          endif()
+
+          message(STATUS "git fetch --all --tags @ ${ABS_PAR_DIR}")
+          execute_process(COMMAND "${GIT_EXECUTABLE}" fetch --all --tags -q
+            RESULT_VARIABLE nok ERROR_VARIABLE error
+            WORKING_DIRECTORY "${ABS_PAR_DIR}")
+          if(nok)
+            message(STATUS "git fetch --all --tags in ${_PAR_DIR} failed:\n ${error}")
+          endif()
+
+      else()
+          message(STATUS "${_PAR_DIR} marked NOREMOTE : Skipping git fetch")
       endif()
 
       # checking out gitref
