1b4dca5d6a19107fd1c5f842df7cccb0a6dcf5f6
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-02-25 15:21:33 +0000

    fix warning in policy 0042

--- cmake/contrib/GreatCMakeCookOff/CheckCXX11Features.cmake
+++ cmake/contrib/GreatCMakeCookOff/CheckCXX11Features.cmake
@@ -17,10 +17,8 @@
 # Original script by Rolf Eike Beer
 # Modifications by Andreas Weis
 # Further Modifications by RSDT@UCL
-CMAKE_MINIMUM_REQUIRED(VERSION 2.8.3)
 
-set(CPP11_FEATURE_CHECK_DIR ${CMAKE_CURRENT_LIST_DIR}/cpp11
-    CACHE INTERNAL "c++11 file directory")
+set(CPP11_FEATURE_CHECK_DIR ${CMAKE_CURRENT_LIST_DIR}/cpp11 CACHE INTERNAL "c++11 file directory")
 
 MACRO(cxx11_check_single_feature FEATURE_NAME FEATURE_NUMBER RESULT_VAR)
 	IF (NOT DEFINED ${RESULT_VAR})
