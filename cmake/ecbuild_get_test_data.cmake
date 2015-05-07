aa22e709002eb467107beeed01f48c337d8a7858
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-05-07 22:19:57 +0100

    Dont download every time

--- cmake/ecbuild_get_test_data.cmake
+++ cmake/ecbuild_get_test_data.cmake
@@ -12,17 +12,12 @@
 
 function( ecbuild_download_resource _p_OUT _p_URL )
 
-    debug_var(_p_OUT)
-    debug_var(_p_URL)
-
-    if( NOT EXISTS ${_p_OUT} )
+    if( NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${_p_OUT} )
 
         find_program( CURL_PROGRAM curl )
 
         execute_process( COMMAND ${CURL_PROGRAM} --silent --show-error --fail --output ${_p_OUT} ${_p_URL} WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} RESULT_VARIABLE CMD_RESULT )
          
-        debug_var(CMD_RESULT)
-
         if(CMD_RESULT)
             message(FATAL_ERROR \"Error downloading ${_p_URL}\")
         endif()
