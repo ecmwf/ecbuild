eb29d9f4c0b7937c67efa7efaf2cf6ce83b8267f
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-08-19 16:17:31 +0100

    ECBUILD-96 ecbuild_add_executable: log debug info

--- cmake/ecbuild_add_executable.cmake
+++ cmake/ecbuild_add_executable.cmake
@@ -51,9 +51,10 @@ macro( ecbuild_add_executable )
       list(REMOVE_DUPLICATES _PAR_INCLUDES )
       foreach( path ${_PAR_INCLUDES} ) # skip NOTFOUND
         if( path )
+          ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): add ${path} to include_directories")
           include_directories( ${path} )
-          #        else()
-          #          message( WARNING "Path ${path} was skipped" )
+        else()
+          ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): ${path} not found - not adding to include_directories")
         endif()
       endforeach()
     endif()
@@ -69,6 +70,7 @@ macro( ecbuild_add_executable )
 
     # remove templates from compilation sources
     if( DEFINED _PAR_TEMPLATES )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): removing ${_PAR_TEMPLATES} from sources")
       list( REMOVE_ITEM _PAR_SOURCES ${_PAR_TEMPLATES} )
       add_custom_target( ${_PAR_TARGET}_templates SOURCES ${_PAR_TEMPLATES} )
     endif()
@@ -79,11 +81,13 @@ macro( ecbuild_add_executable )
     # set OUTPUT_NAME
 
     if( DEFINED _PAR_OUTPUT_NAME )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): set OUTPUT_NAME to ${_PAR_OUTPUT_NAME}")
       set_target_properties( ${_PAR_TARGET} PROPERTIES OUTPUT_NAME ${_PAR_OUTPUT_NAME} )
     endif()
 
     # add extra dependencies
     if( DEFINED _PAR_DEPENDS)
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): add dependency on ${_PAR_DEPENDS}")
       add_dependencies( ${_PAR_TARGET} ${_PAR_DEPENDS} )
     endif()
 
@@ -94,9 +98,10 @@ macro( ecbuild_add_executable )
       list(REMOVE_ITEM _PAR_LIBS optimized)
       foreach( lib ${_PAR_LIBS} ) # skip NOTFOUND
         if( lib )
+          ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): linking with ${lib}")
           target_link_libraries( ${_PAR_TARGET} ${lib} )
         else()
-          #              message( WARNING "Lib ${lib} was skipped" )
+          ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): ${lib} not found - not linking")
         endif()
       endforeach()
     endif()
@@ -106,23 +111,29 @@ macro( ecbuild_add_executable )
 
     # add local flags
     if( DEFINED _PAR_CFLAGS )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): use C flags ${_PAR_CFLAGS}")
       set_source_files_properties( ${${_PAR_TARGET}_c_srcs}   PROPERTIES COMPILE_FLAGS "${_PAR_CFLAGS}" )
     endif()
     if( DEFINED _PAR_CXXFLAGS )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): use C++ flags ${_PAR_CFLAGS}")
       set_source_files_properties( ${${_PAR_TARGET}_cxx_srcs} PROPERTIES COMPILE_FLAGS "${_PAR_CXXFLAGS}" )
     endif()
     if( DEFINED _PAR_FFLAGS )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): use Fortran flags ${_PAR_CFLAGS}")
       set_source_files_properties( ${${_PAR_TARGET}_f_srcs}   PROPERTIES COMPILE_FLAGS "${_PAR_FFLAGS}" )
     endif()
     if( DEFINED _PAR_GENERATED )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): mark as generated")
       set_source_files_properties( ${_PAR_GENERATED} PROPERTIES GENERATED 1 )
     endif()
 
     # define VERSION if requested
     if( DEFINED _PAR_VERSION )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): set version to ${_PAR_VERSION}")
       set_target_properties( ${_PAR_TARGET} PROPERTIES VERSION "${_PAR_VERSION}" )
     else()
       if( _PAR_AUTO_VERSION )
+        ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): set version to ${${PNAME}_MAJOR_VERSION}.${${PNAME}_MINOR_VERSION}")
         set_target_properties( ${_PAR_TARGET} PROPERTIES VERSION "${${PNAME}_MAJOR_VERSION}.${${PNAME}_MINOR_VERSION}" )
       endif()
     endif()
@@ -135,6 +146,7 @@ macro( ecbuild_add_executable )
     # installation
 
     if( NOT _PAR_NOINSTALL )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): installing to ${INSTALL_BIN_DIR}")
 
       # add installation paths and associate with defined component
       #            if( DEFINED _PAR_COMPONENT )
@@ -159,6 +171,7 @@ macro( ecbuild_add_executable )
       export( TARGETS ${_PAR_TARGET} APPEND FILE "${TOP_PROJECT_TARGETS_FILE}" )
 
     else()
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): not installing")
       # NOINSTALL targets are always built the build_rpath, not the install_rpath
       set_property( TARGET ${_PAR_TARGET} PROPERTY SKIP_BUILD_RPATH         FALSE )
       set_property( TARGET ${_PAR_TARGET} PROPERTY BUILD_WITH_INSTALL_RPATH FALSE )
@@ -168,11 +181,13 @@ macro( ecbuild_add_executable )
     if( DEFINED _PAR_DEFINITIONS )
       get_property( _target_defs TARGET ${_PAR_TARGET} PROPERTY COMPILE_DEFINITIONS )
       list( APPEND _target_defs ${_PAR_DEFINITIONS} )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): using definitions ${_target_defs}")
       set_property( TARGET ${_PAR_TARGET} PROPERTY COMPILE_DEFINITIONS ${_target_defs} )
     endif()
 
     # set linker language
     if( DEFINED _PAR_LINKER_LANGUAGE )
+      ecbuild_debug("ecbuild_add_executable(${_PAR_TARGET}): using linker language ${_PAR_LINKER_LANGUAGE}")
       set_property( TARGET ${_PAR_TARGET} PROPERTY LINKER_LANGUAGE ${_PAR_LINKER_LANGUAGE} )
     endif()
 
