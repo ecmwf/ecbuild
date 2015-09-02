48c26848b8bc3abeab67e605a061348ad389e723
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-09-02 15:59:00 +0100

    ecbuild_add_persistent: fix indentation

--- cmake/ecbuild_add_persistent.cmake
+++ cmake/ecbuild_add_persistent.cmake
@@ -9,53 +9,52 @@
 ##############################################################################
 # macro for adding persistent layer object classes
 ##############################################################################
-	
+  
 # define the script to build the persistent class information
 set( sg_perl "${CMAKE_CURRENT_LIST_DIR}/sg.pl" CACHE INTERNAL "perl script to generate persistent objects" )
 
 macro( ecbuild_add_persistent )
 
-	ecbuild_find_perl( REQUIRED )
+  ecbuild_find_perl( REQUIRED )
 
-	set( options )
-    set( single_value_args SRC_LIST NAMESPACE )
-    set( multi_value_args  FILES ) 
+  set( options )
+  set( single_value_args SRC_LIST NAMESPACE )
+  set( multi_value_args  FILES ) 
 
-    cmake_parse_arguments( _PAR "${options}" "${single_value_args}" "${multi_value_args}"  ${_FIRST_ARG} ${ARGN} )
+  cmake_parse_arguments( _PAR "${options}" "${single_value_args}" "${multi_value_args}"  ${_FIRST_ARG} ${ARGN} )
 
-    if(_PAR_UNPARSED_ARGUMENTS)
-      message(FATAL_ERROR "Unknown keywords given to ecbuild_add_persistent(): \"${_PAR_UNPARSED_ARGUMENTS}\"")
+  if(_PAR_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "Unknown keywords given to ecbuild_add_persistent(): \"${_PAR_UNPARSED_ARGUMENTS}\"")
+  endif()
+
+  if( NOT _PAR_SRC_LIST  )
+    message(FATAL_ERROR "The call to ecbuild_add_persistent() doesn't specify the SRC_LIST.")
+  endif()
+
+  if( NOT _PAR_FILES )
+    message(FATAL_ERROR "The call to ecbuild_add_persistent() doesn't specify the FILES.")
+  endif()
+
+  foreach( file ${_PAR_FILES} )
+
+    get_filename_component( _file_dir    ${file} PATH )
+    get_filename_component( _file_we     ${file} NAME_WE )
+
+    set( file ${_file_we} )
+    if( _file_dir )
+      file( MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_file_dir} )
+      set( file ${_file_dir}/${_file_we} )
     endif()
 
-    if( NOT _PAR_SRC_LIST  )
-      message(FATAL_ERROR "The call to ecbuild_add_persistent() doesn't specify the SRC_LIST.")
-    endif()
+    # debug_var(file)
 
-    if( NOT _PAR_FILES )
-      message(FATAL_ERROR "The call to ecbuild_add_persistent() doesn't specify the FILES.")
-    endif()
+    add_custom_command( OUTPUT  ${file}.b
+                        COMMAND ${PERL_EXECUTABLE} ${sg_perl} ${CMAKE_CURRENT_SOURCE_DIR}/${file}.h
+                                ${CMAKE_CURRENT_BINARY_DIR}/${_file_dir} ${_PAR_NAMESPACE}
+                        DEPENDS ${sg_perl} ${file}.h )
+    set_source_files_properties( ${file}.h PROPERTIES OBJECT_DEPENDS "${file}.b" )
+    list( APPEND ${_PAR_SRC_LIST} ${CMAKE_CURRENT_BINARY_DIR}/${file}.b )
 
-    foreach( file ${_PAR_FILES} )
-
-      get_filename_component( _file_dir    ${file} PATH )
-      get_filename_component( _file_we     ${file} NAME_WE )
-
-      set( file ${_file_we} )
-      if( _file_dir )
-         file( MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_file_dir} )
-         set( file ${_file_dir}/${_file_we} )
-      endif()
-
-      # debug_var(file)
-
-      add_custom_command(
-        OUTPUT  ${file}.b
-		COMMAND ${PERL_EXECUTABLE} ${sg_perl} ${CMAKE_CURRENT_SOURCE_DIR}/${file}.h ${CMAKE_CURRENT_BINARY_DIR}/${_file_dir} ${_PAR_NAMESPACE}
-		DEPENDS ${sg_perl} ${file}.h
-        )
-      set_source_files_properties( ${file}.h PROPERTIES OBJECT_DEPENDS "${file}.b" )
-      list( APPEND ${_PAR_SRC_LIST} ${CMAKE_CURRENT_BINARY_DIR}/${file}.b )
-    
-    endforeach()
+  endforeach()
 
 endmacro( ecbuild_add_persistent  )
