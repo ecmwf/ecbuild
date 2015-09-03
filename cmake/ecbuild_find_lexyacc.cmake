fd2501e6626e3618b0905a17111f1cdd5623d95a
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-09-03 13:32:51 +0100

    ecbuild_find_lexyacc: fix indentation

--- cmake/ecbuild_find_lexyacc.cmake
+++ cmake/ecbuild_find_lexyacc.cmake
@@ -19,42 +19,42 @@
 
 macro( ecbuild_find_lexyacc )
 
-	# find preferably bison or else yacc
+  # find preferably bison or else yacc
 
-	if( NOT SKIP_BISON )
+  if( NOT SKIP_BISON )
 
-		find_package( BISON 2.3 )
-		find_package( FLEX )
+    find_package( BISON 2.3 )
+    find_package( FLEX )
 
-	endif()
+  endif()
 
-	if( NOT BISON_FOUND AND NOT SKIP_YACC )
+  if( NOT BISON_FOUND AND NOT SKIP_YACC )
 
-		find_package( YACC )
-		find_package( LEX  )
+    find_package( YACC )
+    find_package( LEX  )
 
-	endif()
+  endif()
 
-	if( NOT YACC_FOUND AND NOT BISON_FOUND ) # neither bison nor yacc were found
-		message( FATAL_ERROR "neither bison or yacc were found - at least one is required (together with its lexical analyser" )
-	endif()
+  if( NOT YACC_FOUND AND NOT BISON_FOUND ) # neither bison nor yacc were found
+    message( FATAL_ERROR "neither bison or yacc were found - at least one is required (together with its lexical analyser" )
+  endif()
 
-	if( NOT YACC_FOUND ) # check for both bison & flex together
-		if( BISON_FOUND AND NOT FLEX_FOUND )
-			message( FATAL_ERROR "both bison and flex are required - flex not found" )
-		endif()
-		if( FLEX_FOUND AND NOT BISON_FOUND )
-			message( FATAL_ERROR "both bison and flex are required - bison not found" )
-		endif()
-	endif()
+  if( NOT YACC_FOUND ) # check for both bison & flex together
+    if( BISON_FOUND AND NOT FLEX_FOUND )
+      message( FATAL_ERROR "both bison and flex are required - flex not found" )
+    endif()
+    if( FLEX_FOUND AND NOT BISON_FOUND )
+      message( FATAL_ERROR "both bison and flex are required - bison not found" )
+    endif()
+  endif()
 
-	if( NOT BISON_FOUND ) # check for both yacc & lex together
-		if( YACC_FOUND AND NOT LEX_FOUND )
-			message( FATAL_ERROR "both yacc and lex are required - lex not found" )
-		endif()
-		if( LEX_FOUND AND NOT YACC_FOUND )
-			message( FATAL_ERROR "both yacc and lex are required - yacc not found" )
-		endif()
-	endif()
+  if( NOT BISON_FOUND ) # check for both yacc & lex together
+    if( YACC_FOUND AND NOT LEX_FOUND )
+      message( FATAL_ERROR "both yacc and lex are required - lex not found" )
+    endif()
+    if( LEX_FOUND AND NOT YACC_FOUND )
+      message( FATAL_ERROR "both yacc and lex are required - yacc not found" )
+    endif()
+  endif()
 
 endmacro( ecbuild_find_lexyacc )
