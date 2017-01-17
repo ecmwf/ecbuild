05a5ab7c921af3f64815f3d435be28b889d93cd5
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2017-01-17 15:54:57 +0000

    ECBUILD-307 curl/wget downloads are not retried by default

--- cmake/ecbuild_download_resource.cmake
+++ cmake/ecbuild_download_resource.cmake
@@ -19,12 +19,17 @@
 # curl or wget is required (curl is preferred if available).
 #
 # The default timeout is 30 seconds, which can be overridden with
-# ``ECBUILD_DOWNLOAD_TIMEOUT``.
+# ``ECBUILD_DOWNLOAD_TIMEOUT``. Downloads are by default only tried once, use
+# ``ECBUILD_DOWNLOAD_RETRIES`` to set the number of retries.
 #
 ##############################################################################
 
 function( ecbuild_download_resource _p_OUT _p_URL )
 
+  # Do not retry downloads by default (ECBUILD-307)
+  if( NOT DEFINED ECBUILD_DOWNLOAD_RETRIES )
+    set( ECBUILD_DOWNLOAD_RETRIES 0 )
+  endif()
   # Use default timeout of 30s if not specified (ECBUILD-307)
   if( NOT DEFINED ECBUILD_DOWNLOAD_TIMEOUT )
     set( ECBUILD_DOWNLOAD_TIMEOUT 30 )
@@ -35,19 +40,31 @@ function( ecbuild_download_resource _p_OUT _p_URL )
     find_program( CURL_PROGRAM curl )
     mark_as_advanced(CURL_PROGRAM)
     if( CURL_PROGRAM )
+
       execute_process( COMMAND ${CURL_PROGRAM} --silent --show-error --fail
+                               --retry ${ECBUILD_DOWNLOAD_RETRIES}
                                --connect-timeout ${ECBUILD_DOWNLOAD_TIMEOUT}
                                --output ${_p_OUT} ${_p_URL}
                        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} RESULT_VARIABLE CMD_RESULT )
+
     else()
+
       find_program( WGET_PROGRAM wget )
+
       if( WGET_PROGRAM )
+
+        # wget takes the total number of tries, curl the number or retries
+        math( EXPR ECBUILD_DOWNLOAD_RETRIES ${ECBUILD_DOWNLOAD_RETRIES} + 1 )
+
         execute_process( COMMAND ${WGET_PROGRAM} -nv -O ${_p_OUT}
+                                 -t ${ECBUILD_DOWNLOAD_RETRIES}
                                  -T ${ECBUILD_DOWNLOAD_TIMEOUT} ${_p_URL}
                          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} RESULT_VARIABLE CMD_RESULT )
+
       else()
         ecbuild_critical("Could not find curl or wget. Error downloading ${_p_URL}")
       endif()
+
     endif()
 
     if(CMD_RESULT)
