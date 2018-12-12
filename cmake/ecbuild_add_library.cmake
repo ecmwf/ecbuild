82bfcba26144b24159c8ab5f8ffac46418c94047
Daniel Tipping <daniel@oldreliable.tech>
2018-12-12 11:47:31 +0000

    Windows: move dlls to build/bin
    This is non-essential: dlls still end up in the install/bin without this
    change. However, without this change dlls end up in build/src which
    doesn't seem like a good place for them.
    
    NB: for tests to find the dll, build/bin needs to be in the system path.

--- cmake/ecbuild_add_library.cmake
+++ cmake/ecbuild_add_library.cmake
@@ -497,8 +497,11 @@ function( ecbuild_add_library_impl )
 
       set_target_properties( ${_PAR_TARGET} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib )
       set_target_properties( ${_PAR_TARGET} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib )
+      if( EC_OS_NAME MATCHES "windows" )
+        set_target_properties( ${_PAR_TARGET} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin )
+      endif()
 
-      # export location of target to other projects -- must be exactly after setting the build location (see previous 2 commands)
+      # export location of target to other projects -- must be exactly after setting the build location (see previous 3 commands)
 
       export( TARGETS ${_PAR_TARGET} APPEND FILE "${TOP_PROJECT_TARGETS_FILE}" )
 
