f4623f22d6fc80f3a7d321254ced5a2fb96063c6
Willem Deconinck <willem.deconinck@ecmwf.int>
2019-01-04 15:45:06 +0000

    ECBUILD-412 [github #3] Fix ecbuild_find_mpi for cmake 3.10 onwards

--- cmake/ecbuild_find_mpi.cmake
+++ cmake/ecbuild_find_mpi.cmake
@@ -84,90 +84,99 @@ macro( ecbuild_find_mpi )
       ecbuild_critical("Unknown keywords given to ecbuild_find_mpi(): \"${_PAR_UNPARSED_ARGUMENTS}\"")
     endif()
 
+    if( NOT _PAR_COMPONENTS )
+      set( _PAR_COMPONENTS C )
+    endif()
+
     # if user defined compilers are MPI compliant, then we use them ...
     if( NOT DEFINED ECBUILD_FIND_MPI )
       set( ECBUILD_FIND_MPI TRUE )
     endif()
     if( ECBUILD_FIND_MPI )
 
-        # C compiler
+        if( CMAKE_VERSION VERSION_LESS 3.10 )
+            # From CMake 3.10 onwards the built-in find_package is good enough, so that
+            # this is no longer required.
 
-        if( CMAKE_C_COMPILER_LOADED AND NOT MPI_C_COMPILER )
+            # C compiler
 
-            include(CheckCSourceCompiles)
+            if( CMAKE_C_COMPILER_LOADED AND NOT MPI_C_COMPILER )
 
-            check_c_source_compiles("
-                #include <mpi.h>
-                int main(int argc, char* argv[])
-                {
-                int rank;
-                MPI_Init(&argc, &argv);
-                MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-                MPI_Finalize();
-                return 0;
-                }
-                "
-                C_COMPILER_SUPPORTS_MPI )
+                include(CheckCSourceCompiles)
 
-            if( C_COMPILER_SUPPORTS_MPI )
-                ecbuild_info( "C compiler supports MPI -- ${CMAKE_C_COMPILER}" )
-                set( MPI_C_COMPILER ${CMAKE_C_COMPILER} )
+                check_c_source_compiles("
+                    #include <mpi.h>
+                    int main(int argc, char* argv[])
+                    {
+                    int rank;
+                    MPI_Init(&argc, &argv);
+                    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+                    MPI_Finalize();
+                    return 0;
+                    }
+                    "
+                    C_COMPILER_SUPPORTS_MPI )
+
+                if( C_COMPILER_SUPPORTS_MPI )
+                    ecbuild_info( "C compiler supports MPI -- ${CMAKE_C_COMPILER}" )
+                    set( MPI_C_COMPILER ${CMAKE_C_COMPILER} )
+                endif()
+
             endif()
 
-        endif()
+            # CXX compiler
 
-        # CXX compiler
+            if( CMAKE_CXX_COMPILER_LOADED AND NOT MPI_CXX_COMPILER )
 
-        if( CMAKE_CXX_COMPILER_LOADED AND NOT MPI_CXX_COMPILER )
+                include(CheckCXXSourceCompiles)
 
-            include(CheckCXXSourceCompiles)
+                check_cxx_source_compiles("
+                    #include <mpi.h>
+                     #include <iostream>
+                     int main(int argc, char* argv[])
+                     {
+                       MPI_Init(&argc, &argv); int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank); MPI_Finalize();
+                       return 0;
+                     }
+                     "
+                     CXX_COMPILER_SUPPORTS_MPI )
 
-            check_cxx_source_compiles("
-                #include <mpi.h>
-                 #include <iostream>
-                 int main(int argc, char* argv[])
-                 {
-                   MPI_Init(&argc, &argv); int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank); MPI_Finalize();
-                   return 0;
-                 }
-                 "
-                 CXX_COMPILER_SUPPORTS_MPI )
+                if( CXX_COMPILER_SUPPORTS_MPI )
+                    ecbuild_info( "C++ compiler supports MPI -- ${CMAKE_CXX_COMPILER}" )
+                    set( MPI_CXX_COMPILER ${CMAKE_CXX_COMPILER} )
+                endif()
 
-            if( CXX_COMPILER_SUPPORTS_MPI )
-                ecbuild_info( "C++ compiler supports MPI -- ${CMAKE_CXX_COMPILER}" )
-                set( MPI_CXX_COMPILER ${CMAKE_CXX_COMPILER} )
             endif()
 
-        endif()
+            # Fortran compiler
 
-        # Fortran compiler
+            if( CMAKE_Fortran_COMPILER_LOADED AND NOT MPI_Fortran_COMPILER )
 
-        if( CMAKE_Fortran_COMPILER_LOADED AND NOT MPI_Fortran_COMPILER )
+                include(CheckFortranSourceCompiles)
 
-            include(CheckFortranSourceCompiles)
+                check_fortran_source_compiles("
+                    program main
+                    use MPI
+                    integer ierr
+                    call MPI_INIT( ierr )
+                    call MPI_FINALIZE( ierr )
+                    end
+                    "
+                Fortran_COMPILER_SUPPORTS_MPI )
 
-            check_fortran_source_compiles("
-                program main
-                use MPI
-                integer ierr
-                call MPI_INIT( ierr )
-                call MPI_FINALIZE( ierr )
-                end
-                "
-            Fortran_COMPILER_SUPPORTS_MPI )
+                if( Fortran_COMPILER_SUPPORTS_MPI )
+                    ecbuild_info( "Fortran compiler supports MPI (F90) -- ${CMAKE_Fortran_COMPILER}" )
+                    set( MPI_Fortran_COMPILER ${CMAKE_Fortran_COMPILER} )
+                    set( MPI_Fortran_FOUND TRUE )
+                endif()
 
-            if( Fortran_COMPILER_SUPPORTS_MPI )
-                ecbuild_info( "Fortran compiler supports MPI (F90) -- ${CMAKE_Fortran_COMPILER}" )
-                set( MPI_Fortran_COMPILER ${CMAKE_Fortran_COMPILER} )
-                set( MPI_Fortran_FOUND TRUE )
             endif()
-
         endif()
 
         if( NOT _PAR_REQUIRED )
-            find_package( MPI QUIET )
+            find_package( MPI COMPONENTS ${_PAR_COMPONENTS} )
         else()
-            find_package( MPI QUIET REQUIRED )
+            find_package( MPI REQUIRED COMPONENTS ${_PAR_COMPONENTS} )
         endif()
 
         if( C_COMPILER_SUPPORTS_MPI )
@@ -182,7 +191,6 @@ macro( ecbuild_find_mpi )
 
     else()
 
-        # find_package with Cray compiler did not send MPI_<lang>_FOUND
         if( CMAKE_C_COMPILER_LOADED )
             set( C_COMPILER_SUPPORTS_MPI TRUE )
             set( MPI_C_FOUND TRUE )
@@ -202,14 +210,13 @@ macro( ecbuild_find_mpi )
 
     mark_as_advanced( MPI_LIBRARY MPI_EXTRA_LIBRARY )
 
-    if( NOT _PAR_COMPONENTS )
-      set( _PAR_COMPONENTS C )
-    endif()
-
     set( MPI_FOUND TRUE )
     foreach( _lang ${_PAR_COMPONENTS} )
       if( NOT MPI_${_lang}_FOUND )
         set( MPI_FOUND FALSE )
+      endif()
+      if( NOT MPI_${_lang}_INCLUDE_DIRS )
+        set( MPI_${_lang}_INCLUDE_DIRS ${MPI_${_lang}_INCLUDE_PATH} )
       endif()
     endforeach()
 
@@ -266,18 +273,18 @@ macro( ecbuild_enable_mpi )
 
     if( MPI_C_FOUND AND NOT C_COMPILER_SUPPORTS_MPI )
         ecbuild_add_c_flags("${MPI_C_COMPILE_FLAGS}")
-        include_directories(${MPI_C_INCLUDE_PATH})
+        include_directories(${MPI_C_INCLUDE_DIRS})
     endif()
 
     if( MPI_CXX_FOUND AND NOT CXX_COMPILER_SUPPORTS_MPI )
         ecbuild_add_cxx_flags("${MPI_CXX_COMPILE_FLAGS}")
-        include_directories(${MPI_CXX_INCLUDE_PATH})
+        include_directories(${MPI_CXX_INCLUDE_DIRS})
     endif()
 
     if( MPI_Fortran_FOUND AND NOT Fortran_COMPILER_SUPPORTS_MPI )
         include( ecbuild_check_fortran_source_return )
         ecbuild_add_fortran_flags("${MPI_Fortran_COMPILE_FLAGS}")
-        include_directories(${MPI_Fortran_INCLUDE_PATH})
+        include_directories(${MPI_Fortran_INCLUDE_DIRS})
     endif()
 
 endmacro( ecbuild_enable_mpi )
@@ -310,19 +317,19 @@ macro( ecbuild_include_mpi )
     if( MPI_C_FOUND AND NOT C_COMPILER_SUPPORTS_MPI )
         include( ecbuild_check_c_source_return )
         ecbuild_add_c_flags("${MPI_C_COMPILE_FLAGS}")
-        include_directories(${MPI_C_INCLUDE_PATH})
+        include_directories(${MPI_C_INCLUDE_DIRS})
     endif()
 
     if( MPI_CXX_FOUND AND NOT CXX_COMPILER_SUPPORTS_MPI )
         include( ecbuild_check_cxx_source_return )
         ecbuild_add_cxx_flags("${MPI_CXX_COMPILE_FLAGS}")
-        include_directories(${MPI_CXX_INCLUDE_PATH})
+        include_directories(${MPI_CXX_INCLUDE_DIRS})
     endif()
 
     if( MPI_Fortran_FOUND AND NOT Fortran_COMPILER_SUPPORTS_MPI )
         include( ecbuild_check_fortran_source_return )
         ecbuild_add_fortran_flags("${MPI_Fortran_COMPILE_FLAGS}")
-        include_directories(${MPI_Fortran_INCLUDE_PATH})
+        include_directories(${MPI_Fortran_INCLUDE_DIRS})
     endif()
 
 endmacro( ecbuild_include_mpi )
