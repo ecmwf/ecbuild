6e297095ec1aaf040249b38fcdd68366b64d9a3b
Daniel Tipping <daniel@oldreliable.tech>
2019-01-08 16:59:22 +0000

    Prevent bash usage for EXE tests

--- cmake/ecbuild_add_test.cmake
+++ cmake/ecbuild_add_test.cmake
@@ -460,6 +460,8 @@ macro( ecbuild_add_test )
       if( EC_OS_NAME MATCHES "windows" AND ${_PAR_TYPE} MATCHES "SCRIPT" )
         # Windows has to be explicitly told to use bash for the tests.
         set( _WIN_CMD "bash" "--rcfile" "${CMAKE_CURRENT_SOURCE_DIR}/windows_testing.bashrc" "-ci" )
+      else()
+        set( _WIN_CMD "" )
       endif()
 
       if( DEFINED _PAR_COMMAND )
