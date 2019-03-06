616102078b8dd203862b4f00fac2f401db8f5286
Willem Deconinck <willem.deconinck@ecmwf.int>
2019-03-06 18:58:04 +0000

    ECBUILD-423 Improve version handling

--- cmake/VERSION.cmake
+++
@@ -1,4 +0,0 @@
-set( ecbuild_VERSION_MAJOR "2" )
-set( ecbuild_VERSION_MINOR "10" )
-set( ecbuild_VERSION_PATCH "1" )
-set( ecbuild_VERSION "${ecbuild_VERSION_MAJOR}.${ecbuild_VERSION_MINOR}.${ecbuild_VERSION_PATCH}")
