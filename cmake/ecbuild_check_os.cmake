48887acd725f63902c3cfc4c5e02c5ba586449ed
Daniel Tipping <daniel@oldreliable.tech>
2019-03-11 11:12:28 +0000

    Windows: add absolute path to m2-bash
    execute_process() executes the child process using operating system APIs
    directly. This means that on Windows 10 with WSL installed, cmake was
    using WSL bash instead of conda-installed m2-bash. WSL bash is unable to
    parse Windows style paths which was causing a 'file not found' error.

--- cmake/ecbuild_check_os.cmake
+++ cmake/ecbuild_check_os.cmake
@@ -426,7 +426,8 @@ if( WIN32 )
 
   ecbuild_warn( "CMake doesn't support symlinks on Windows. "
                 "Replacing all symlinks with copies." )
-  execute_process( COMMAND bash -c "${ECBUILD_MACROS_DIR}/ecbuild_windows_replace_symlinks.sh"
+  set( BASH_EXE "$ENV{CONDA_PREFIX}/Library/usr/bin/bash.exe" )
+  execute_process( COMMAND ${BASH_EXE} -c "${ECBUILD_MACROS_DIR}/ecbuild_windows_replace_symlinks.sh"
                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                    RESULT_VARIABLE CMD_RESULT
                    OUTPUT_VARIABLE CMD_OUTPUT
