3aeb67155bc941f2518a8cc730aa5f296a463beb
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2019-04-23 12:19:12 +0100

    Fix ecbuild --version output

--- cmake/VERSION.cmake
+++ cmake/VERSION.cmake
@@ -1,3 +1,5 @@
+# WARNING: this file is parsed by the ecbuild script, please use only literal
+# numbers (not variables) in ecbuild_VERSION_*
 set( ecbuild_VERSION_MAJOR "2" )
 set( ecbuild_VERSION_MINOR "10" )
 set( ecbuild_VERSION_PATCH "2" )
