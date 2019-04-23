24da42b37c6404af07e0770f40b00dc86ba67e41
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2019-04-23 12:31:06 +0100

    Bump version

--- cmake/VERSION.cmake
+++ cmake/VERSION.cmake
@@ -1,6 +1,6 @@
 # WARNING: this file is parsed by the ecbuild script, please use only literal
 # numbers (not variables) in ecbuild_VERSION_*
-set( ecbuild_VERSION_MAJOR "2" )
-set( ecbuild_VERSION_MINOR "10" )
-set( ecbuild_VERSION_PATCH "2" )
+set( ecbuild_VERSION_MAJOR "3" )
+set( ecbuild_VERSION_MINOR "0" )
+set( ecbuild_VERSION_PATCH "0" )
 set( ecbuild_VERSION "${ecbuild_VERSION_MAJOR}.${ecbuild_VERSION_MINOR}.${ecbuild_VERSION_PATCH}")
