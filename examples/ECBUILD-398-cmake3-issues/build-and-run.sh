646acf6b089b14de37aabf36c3b395833a23ef9b
Willem Deconinck <willem.deconinck@ecmwf.int>
2019-05-02 14:46:48 +0100

    ECBUILD-423 Update some examples to test new project()

--- examples/ECBUILD-398-cmake3-issues/build-and-run.sh
+++ examples/ECBUILD-398-cmake3-issues/build-and-run.sh
@@ -15,7 +15,7 @@ cd $HERE/projectA
 
 mkdir build
 cd build
-ecbuild --prefix=../install -- ../
+ecbuild --prefix=../install -- -Wno-deprecated -DECBUILD_2_COMPAT=OFF ../
 make install
 
 # ----------------- build projectB ---------------------
@@ -25,7 +25,7 @@ mkdir build
 cd build
 ecbuild --prefix=../install -- \
     -DprojectA_DIR=$HERE/projectA/install/lib/cmake/projectA \
-    ../
+    -Wno-deprecated -DECBUILD_2_COMPAT=OFF ../
 make install
 
 # ----------------- build projectC ---------------------
@@ -36,7 +36,7 @@ cd build
 ecbuild --prefix=../install -- \
     -DprojectA_DIR=$HERE/projectA/install/lib/cmake/projectA \
     -DprojectB_DIR=$HERE/projectB/install/lib/cmake/projectB \
-    ../
+    -Wno-deprecated -DECBUILD_2_COMPAT=OFF ../
 make install
 
 # ----------------- Run ---------------------
