8b6467b169288b13bbaba953415c7e564f32cdab
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2019-05-03 16:38:47 +0100

    Rework the project() wrapper

--- cmake/ecbuild_project.cmake
+++ cmake/ecbuild_project.cmake
@@ -10,12 +10,10 @@ if( NOT ECBUILD_PROJECT_INCLUDED )
 set( ECBUILD_PROJECT_INCLUDED TRUE )
 
 
+# XXX: CMake apparently parses the main CMakeLists.txt looking for a direct call
+# to project(), which means we cannot just create an ecbuild_project, hence the
+# override.
 macro( project _project_name )
-
-  cmake_policy(PUSH)
-
-  cmake_minimum_required(VERSION 3.3 FATAL_ERROR) # for using IN_LIST
-  cmake_policy(SET CMP0057 NEW) # for using IN_LIST
 
   if( ECBUILD_PROJECT_${CMAKE_CURRENT_SOURCE_DIR} OR ECBUILD_PROJECT_${_project_name} )
 
@@ -37,20 +35,13 @@ macro( project _project_name )
       ecbuild_parse_version( "${_ecbuild_${_project_name}_VERSION}" PREFIX ${_project_name} )
     elseif( EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION )
       ecbuild_parse_version_file( "VERSION" PREFIX ${_project_name} )
-    elseif( EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/VERSION.cmake )
-      set( PROJECT_NAME ${_project_name} )
-      include( ${CMAKE_CURRENT_SOURCE_DIR}/VERSION.cmake )
-      if( ${_project_name}_VERSION_STR )
-        if(ECBUILD_2_COMPAT)
-          if(ECBUILD_2_COMPAT_DEPRECATE)
-            ecbuild_deprecate("Please create the file\n\t${CMAKE_CURRENT_SOURCE_DIR}/VERSION\ncontaining the version string '${${_project_name}_VERSION_STR}'")
-          endif()
-        else()
-          ecbuild_critical("Please create the file\n\t${CMAKE_CURRENT_SOURCE_DIR}/VERSION\ncontaining the version string '${${_project_name}_VERSION_STR}'")
-        endif()
-        ecbuild_parse_version( "${${_project_name}_VERSION_STR}" PREFIX ${_project_name} )
-        endif()
+    else()
+      ecbuild_critical("Please specify a version for project ${_project_name}")
     endif()
+
+    cmake_policy(PUSH)
+    cmake_minimum_required(VERSION 3.3 FATAL_ERROR) # for using IN_LIST
+    cmake_policy(SET CMP0057 NEW) # for using IN_LIST
 
     unset( _require_LANGUAGES )
     foreach( _lang C CXX Fortran )
@@ -68,7 +59,9 @@ macro( project _project_name )
       endif()
       list( INSERT _ecbuild_${_project_name}_UNPARSED_ARGUMENTS 0 "LANGUAGES" )
     endif()
-    
+
+    cmake_policy(POP)
+
     if( ${_project_name}_VERSION_STR )
       cmake_policy(SET CMP0048 NEW )
       _project( ${_project_name} VERSION ${${_project_name}_VERSION} ${_ecbuild_${_project_name}_UNPARSED_ARGUMENTS} )
@@ -83,9 +76,14 @@ macro( project _project_name )
 
     ecbuild_declare_project()
 
-    else()
+  else() # ecbuild 2 or pure CMake
 
-      ecbuild_debug( "CMake project(${_project_name}) ")
+    ecbuild_debug( "CMake project(${_project_name}) ")
+
+    if(ECBUILD_2_COMPAT)
+      cmake_policy(PUSH)
+      cmake_minimum_required(VERSION 3.3 FATAL_ERROR) # for using IN_LIST
+      cmake_policy(SET CMP0057 NEW) # for using IN_LIST
 
       unset( _args )
       foreach( arg ${ARGN} )
@@ -93,16 +91,20 @@ macro( project _project_name )
       endforeach()
 
       if( VERSION IN_LIST _args )
-        cmake_policy(SET CMP0048 NEW )
+        set(_cmp0048_val NEW)
       else()
-        cmake_policy(SET CMP0048 OLD )
+        set(_cmp0048_val OLD)
       endif()
 
-      _project( ${_project_name} ${ARGN} )
+      cmake_policy(POP)
+
+      cmake_policy(SET CMP0048 ${_cmp0048_val} )
+      unset(_cmp0048_val)
+    endif()
+
+    _project( ${_project_name} ${ARGN} )
 
   endif()
-
-  cmake_policy(POP)
 
 endmacro()
 
