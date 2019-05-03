b7be9957c8a2f359648593606bb347f0b3ec7cb3
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2019-05-03 13:46:04 +0100

    Use ecbuild_parse_version for compat VERSION.cmake support

--- cmake/compat/ecbuild_compat_setversion.cmake
+++ cmake/compat/ecbuild_compat_setversion.cmake
@@ -22,8 +22,10 @@
 # * <pname>_VERSION_MAJOR
 # * <pname>_VERSION_MINOR
 # * <pname>_VERSION_PATCH
+# * <pname>_VERSION_TWEAK
 # * <pname>_VERSION
 # * <pname>_VERSION_STR
+# * <pname>_VERSION_SUFFIX
 #
 ##############################################################################
 
@@ -36,32 +38,8 @@ macro(ecbuild_compat_setversion)
     set( __version "0.0.0" )
   endif()
 
-  # Remove any non-numbers
-  string( REGEX REPLACE "^((([0-9]+)\\.)+([0-9]+)).*" "\\1" __version "${__version}" )
+  ecbuild_parse_version("${__version}" PREFIX ${PROJECT_NAME})
 
-  string( REPLACE "." " " _version_list "${__version}" ) # dots to spaces
-
-  separate_arguments( _version_list )
-  list( LENGTH _version_list _len )
-  set( __version "" )
-  if( ${_len} GREATER 0 )
-    list( GET _version_list 0 ${PROJECT_NAME}_VERSION_MAJOR )
-    set( __version "${${PROJECT_NAME}_VERSION_MAJOR}" )
-  endif()
-  if( ${_len} GREATER 1 )
-    list( GET _version_list 1 ${PROJECT_NAME}_VERSION_MINOR )
-    set( __version "${__version}.${${PROJECT_NAME}_VERSION_MINOR}" )
-  endif()
-  if( ${_len} GREATER 2 )
-    list( GET _version_list 2 ${PROJECT_NAME}_VERSION_PATCH )
-    set( __version "${__version}.${${PROJECT_NAME}_VERSION_PATCH}" )
-  endif()
-  if( ${_len} GREATER 3 )
-    list( GET _version_list 3 ${PROJECT_NAME}_VERSION_TWEAK )
-    set( __version "${__version}.${${PROJECT_NAME}_VERSION_TWEAK}" )
-  endif()
-
-  set( ${PROJECT_NAME}_VERSION "${__version}")
   set( ${PROJECT_NAME}_VERSION "${${PROJECT_NAME}_VERSION}" CACHE INTERNAL "package ${PROJECT_NAME} version" )
   set( ${PROJECT_NAME}_VERSION_STR "${${PROJECT_NAME}_VERSION}" CACHE INTERNAL "package ${PROJECT_NAME} version" )
 endmacro()
