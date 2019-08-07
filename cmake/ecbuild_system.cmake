b8f7cc501710957c8b2b229fe38ca0ea6cdbe864
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2019-08-07 16:35:50 +0100

    Add ecbuild_string for extra string manipulations

--- cmake/ecbuild_system.cmake
+++ cmake/ecbuild_system.cmake
@@ -178,6 +178,8 @@ if( PROJECT_NAME STREQUAL CMAKE_PROJECT_NAME )
 
     include( ecbuild_evaluate_dynamic_condition )
     include( ecbuild_filter_list )
+
+    include( ecbuild_string )
     include( ecbuild_parse_version )
 
     include( ecbuild_list_macros )
