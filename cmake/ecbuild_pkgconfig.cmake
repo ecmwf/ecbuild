9a472a2681387b9beb0c4f9ae0e7424e3f3831e2
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2019-08-08 12:34:30 +0100

    ECBUILD-440 Escape paths before using them in a regex in ecbuild_pkgconfig

--- cmake/ecbuild_pkgconfig.cmake
+++ cmake/ecbuild_pkgconfig.cmake
@@ -228,11 +228,16 @@ function( ecbuild_pkgconfig_include INCLUDE INCLUDE_DIRS ignore_includes )
      "\\$<INSTALL_INTERFACE"  # Ignore generator expressions
      ${_ignore_includes}
   )
+  set( ignore_include_dirs_escaped )
+  foreach( _ignore ${ignore_include_dirs} )
+    ecbuild_string( REGEX ESCAPE "${_ignore}" _ignore_escaped )
+    list( APPEND ignore_include_dirs_escaped "${_ignore_escaped}" )
+  endforeach()
 
   foreach( _incdir ${${INCLUDE_DIRS}} )
 
-    foreach( _ignore ${ignore_include_dirs} )
-      if( "${_incdir}" STREQUAL "${_ignore}" )
+    foreach( _ignore ${ignore_include_dirs_escaped} )
+      if( "${_incdir}" MATCHES "${_ignore}" )
         unset( _incdir )
         break()
       endif()
