630bdd46931422dfabac9af39d8d0e9f90e058d5
Wojciech Smigaj <wojciech.smigaj@metoffice.gov.uk>
2020-06-04 13:01:09 +0100

    Add an option controlling generation of the *_GIT_SHA1 variables.

--- cmake/ecbuild_define_options.cmake
+++ cmake/ecbuild_define_options.cmake
@@ -35,6 +35,8 @@ mark_as_advanced( ECBUILD_INSTALL_LIBRARY_HEADERS )
 option( ECBUILD_INSTALL_FORTRAN_MODULES "Will install Fortran modules" ON )
 mark_as_advanced( ECBUILD_INSTALL_FORTRAN_MODULES )
 
+option( RECORD_GIT_SHA1_IN_VARIABLES "When building ecbuild projects that are Git repos, create variables recording the full and short Git revision" ON )
+
 include( CMakeDependentOption ) # make options depend on one another
 
 set( CMAKE_NO_SYSTEM_FROM_IMPORTED ON )
