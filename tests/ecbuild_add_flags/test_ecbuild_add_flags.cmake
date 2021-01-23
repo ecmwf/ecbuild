475d7d6bcc83757d6c8191eab020c5af40197990
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-01-23 12:00:30 +0000

    ECBUILD-504 fix tests

--- tests/ecbuild_add_flags/test_ecbuild_add_flags.cmake
+++ tests/ecbuild_add_flags/test_ecbuild_add_flags.cmake
@@ -1,16 +1,18 @@
 cmake_minimum_required( VERSION 3.6 FATAL_ERROR )
 
 find_package( ecbuild REQUIRED )
-include( ecbuild_add_flags )
 
-set(testno 1)
-foreach(str "bla" "1.2" "a(b)" "a[b]" "x++" "a\\0" "x*" "x?" "a|b" "$v" "^a")
-    message("Escaping '${str}'")
-    ecbuild_regex_escape("${str}" test${testno})
-    message("-> ${test${testno}}")
-    string(REGEX REPLACE "${test${testno}}" "match" test${testno}_match "${str}")
-    if(NOT "${test${testno}_match}" STREQUAL "match")
-      message(FATAL_ERROR "ecbuild_regex_escape not working as expected")
-    endif()
-    math(EXPR testno "${testno} + 1")
-endforeach()
+ecbuild_add_c_flags( "-O2" )              # should be able to add to (nearly) all compilers
+ecbuild_add_c_flags( "-fooxxx" NO_FAIL )  # should not add to any compiler
+
+if( CMAKE_C_FLAGS MATCHES "-O2" )
+  message("Flag -O2 added")
+else()
+  message(FATAL_ERROR "Failed to add C flag -O2" )
+endif()
+
+if( CMAKE_C_FLAGS MATCHES "-fooxxx" )
+  message(FATAL_ERROR "Flag -fooxxx wrongly added" )
+else()
+  message("Successfully skiped addition of fake C flag -fooxxx")
+endif()
