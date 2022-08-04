7cb0d4e6aa385aa8480e9cab6af2e1de8840c22e
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2022-08-04 15:49:38 +0000

    Change the default ECBUILD_DOWNLOAD_BASE_URL
    The test data repository root will move to https://get.ecmwf.int/repository/test-data

--- cmake/ecbuild_get_test_data.cmake
+++ cmake/ecbuild_get_test_data.cmake
@@ -141,7 +141,7 @@ endfunction()
 # Download test data from ``<ECBUILD_DOWNLOAD_BASE_URL>/<DIRNAME>/<NAME>``
 #
 # If the ``ECBUILD_DOWNLOAD_BASE_URL`` variable is not set, the default URL
-# ``http://download.ecmwf.org/test-data`` is used.
+# ``https://get.ecmwf.int/repository/test-data`` is used.
 #
 # If the ``DIRNAME`` argument is not given, test data will be downloaded
 # from ``<ECBUILD_DOWNLOAD_BASE_URL>/<project>/<relative path to current dir>/<NAME>``
@@ -203,7 +203,7 @@ function( ecbuild_get_test_data )
 
     # Allow the user to override the base download URL (ECBUILD-447)
     if( NOT DEFINED ECBUILD_DOWNLOAD_BASE_URL )
-      set( ECBUILD_DOWNLOAD_BASE_URL http://download.ecmwf.org/test-data )
+      set( ECBUILD_DOWNLOAD_BASE_URL https://get.ecmwf.int/repository/test-data )
     endif()
 
     # Set download URL
@@ -355,7 +355,7 @@ endfunction(ecbuild_get_test_data)
 # md5 checksum, separated with a ``:`` (the name must not contain spaces).
 #
 # If the ``ECBUILD_DOWNLOAD_BASE_URL`` variable is not set, the default URL
-# ``http://download.ecmwf.org/test-data`` is used.
+# ``https://get.ecmwf.int/repository/test-data`` is used.
 #
 # If the ``DIRNAME`` argument is not given, test data will be downloaded
 # from ``<ECBUILD_DOWNLOAD_BASE_URL>/<project>/<relative path to current dir>/<NAME>``
