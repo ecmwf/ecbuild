e9c6dd27dbc0d45d4b2992073c836f98ae8b3f36
marcosbento <Marcos.Bento@ecmwf.int>
2025-03-19 08:17:42 +0000

    Determine the ON/OFF status of an option only at the end (#82)
    https://github.com/ecmwf/ecbuild/issues/79

--- tests/project_summary/configure.sh
+++ tests/project_summary/configure.sh
@@ -27,20 +27,41 @@ $SOURCE/clean.sh
 
 # ----------------- configure project ---------------------
 
-# Options: -DENABLE_MYFEATURE=ON
+# Options: -DENABLE_FEATURE_A=ON
 
 mkdir -p $HERE/build_1
-ecbuild -DENABLE_MYFEATURE=ON $SOURCE/test_project -B $HERE/build_1 | tee $HERE/build_1.log
-EXPECT_ONE_OF $HERE/build_1.log "* MYFEATURE, proja(ON): '', projb(ON): ''"
+ecbuild -DENABLE_FEATURE_A=ON $SOURCE/test_project -B $HERE/build_1 | tee $HERE/build_1.log
+EXPECT_ONE_OF $HERE/build_1.log "* FEATURE_A, proja(ON): '', projb(ON): ''"
 
-# Options: -DENABLE_MYFEATURE=ON -DPROJB_ENABLE_MYFEATURE=OFF
+# Options: -DENABLE_FEATURE_A=ON -DPROJB_ENABLE_FEATURE_A=OFF
 
 mkdir -p $HERE/build_2
-ecbuild -DENABLE_MYFEATURE=ON -DPROJB_ENABLE_MYFEATURE=OFF $SOURCE/test_project -B $HERE/build_2 | tee $HERE/build_2.log
-EXPECT_ONE_OF $HERE/build_2.log "* MYFEATURE, proja(ON): '', projb(OFF): ''"
+ecbuild -DENABLE_FEATURE_A=ON -DPROJB_ENABLE_FEATURE_A=OFF $SOURCE/test_project -B $HERE/build_2 | tee $HERE/build_2.log
+EXPECT_ONE_OF $HERE/build_2.log "* FEATURE_A, proja(ON): '', projb(OFF): ''"
 
-# Options: -DENABLE_MYFEATURE=OFF -DPROJB_ENABLE_MYFEATURE=ON
+# Options: -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DSOME_PACKAGE_FOUND=OFF
  
 mkdir -p $HERE/build_3
-ecbuild -DENABLE_MYFEATURE=OFF -DPROJB_ENABLE_MYFEATURE=ON $SOURCE/test_project -B $HERE/build_3 | tee $HERE/build_3.log
-EXPECT_ONE_OF $HERE/build_3.log "* MYFEATURE, proja(OFF): '', projb(ON): ''"
+ecbuild -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DSOME_PACKAGE_FOUND=OFF $SOURCE/test_project -B $HERE/build_3 | tee $HERE/build_3.log
+EXPECT_ONE_OF $HERE/build_3.log "* FEATURE_A, proja(OFF): '', projb(ON): ''"
+EXPECT_ONE_OF $HERE/build_3.log "* FEATURE_B, projb(OFF): ''"
+
+# Options: -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DSOME_PACKAGE_FOUND=ON
+ 
+mkdir -p $HERE/build_4
+ecbuild -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DSOME_PACKAGE_FOUND=ON $SOURCE/test_project -B $HERE/build_4 | tee $HERE/build_4.log
+EXPECT_ONE_OF $HERE/build_4.log "* FEATURE_A, proja(OFF): '', projb(ON): ''"
+EXPECT_ONE_OF $HERE/build_4.log "* FEATURE_B, projb(ON): ''"
+
+# Options: -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DENABLE_FEATURE_B=ON -DSOME_PACKAGE_FOUND=ON
+ 
+mkdir -p $HERE/build_5
+ecbuild -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DENABLE_FEATURE_B=ON -DSOME_PACKAGE_FOUND=ON $SOURCE/test_project -B $HERE/build_5 | tee $HERE/build_5.log
+EXPECT_ONE_OF $HERE/build_5.log "* FEATURE_A, proja(OFF): '', projb(ON): ''"
+EXPECT_ONE_OF $HERE/build_5.log "* FEATURE_B, projb(ON): ''"
+
+# Options: -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DENABLE_FEATURE_B=ON -DSOME_PACKAGE_FOUND=OFF
+ 
+mkdir -p $HERE/build_6
+ecbuild -DENABLE_FEATURE_A=OFF -DPROJB_ENABLE_FEATURE_A=ON -DENABLE_FEATURE_B=ON -DSOME_PACKAGE_FOUND=OFF $SOURCE/test_project -B $HERE/build_6 | tee $HERE/build_6.log
+EXPECT_ONE_OF $HERE/build_6.log "Configuring incomplete, errors occurred!"
