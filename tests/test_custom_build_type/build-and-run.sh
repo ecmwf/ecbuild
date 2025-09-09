00f3e5eedf799566d6a70c5c188cfd6aa4ed1db8
Marcos Bento <Marcos.Bento@ecmwf.int>
2025-09-09 10:33:30 +0100

    Improve test for custom build type
    - Check common/custom C and C++ compilation options
    - Add test for variables provided as ECBUILD_* variables

--- tests/test_custom_build_type/build-and-run.sh
+++ tests/test_custom_build_type/build-and-run.sh
@@ -11,15 +11,57 @@ export PATH=$SOURCE/../../bin:$PATH
 echo $PATH
 echo $SOURCE
 
-# Build the project
-mkdir -p $HERE/build
-cd $HERE/build
-cmake $SOURCE/test_project -DCMAKE_BUILD_TYPE=CUSTOM -DCMAKE_TOOLCHAIN_FILE=$SOURCE/toolchain.cmake
+function assert_flag() {
+  local file=$1
+  local variable=$2
+  local flag=$3
+  local found=$(grep "${variable} = " ${file} | grep "\\${flag}" | wc -l)
 
-flags=$(grep Fortran_FLAGS CMakeFiles/proj.dir/flags.make | grep '\-common-flag -custom-flag' | wc -l)
+  if [ $found -ne 1 ]; then
+      echo "File ${file} does not contain exacly one of '$3'"
+      exit 1
+  fi
+}
 
-if [ $flags -ne 1 ]
-then
-  exit 1
-fi
+# 1) Build the project -- using toolchain
 
+mkdir -p $HERE/build-1
+cd $HERE/build-1
+cmake $SOURCE/test_project \
+  -DCMAKE_BUILD_TYPE=CUSTOM \
+  -DCMAKE_TOOLCHAIN_FILE=$SOURCE/toolchain.cmake
+
+# Check that the flags are correctly set for Fortran, C and C++
+
+assert_flag CMakeFiles/proj_f.dir/flags.make "Fortran_FLAGS" "-Wno-common-f-flag"
+assert_flag CMakeFiles/proj_f.dir/flags.make "Fortran_FLAGS" "-Wno-custom-f-flag"
+
+assert_flag CMakeFiles/proj_c.dir/flags.make "C_FLAGS" "-Wno-common-c-flag"
+assert_flag CMakeFiles/proj_c.dir/flags.make "C_FLAGS" "-Wno-custom-c-flag"
+
+assert_flag CMakeFiles/proj_cxx.dir/flags.make "CXX_FLAGS" "-Wno-common-cxx-flag"
+assert_flag CMakeFiles/proj_cxx.dir/flags.make "CXX_FLAGS" "-Wno-custom-cxx-flag"
+
+# 2) Build the project -- using explicit ECBUILD_*_FLAGS_<type> variables
+
+mkdir -p $HERE/build-2
+cd $HERE/build-2
+cmake $SOURCE/test_project \
+  -DCMAKE_BUILD_TYPE=CUSTOM \
+  -DECBUILD_Fortran_FLAGS="-Wno-common-f-flag" \
+  -DECBUILD_C_FLAGS="-Wno-common-c-flag" \
+  -DECBUILD_CXX_FLAGS="-Wno-common-cxx-flag" \
+  -DECBUILD_Fortran_FLAGS_CUSTOM="-Wno-custom-f-flag" \
+  -DECBUILD_C_FLAGS_CUSTOM="-Wno-custom-c-flag" \
+  -DECBUILD_CXX_FLAGS_CUSTOM="-Wno-custom-cxx-flag"
+
+# Check that the flags are correctly set for Fortran, C and C++
+
+assert_flag CMakeFiles/proj_f.dir/flags.make "Fortran_FLAGS" "-Wno-common-f-flag"
+assert_flag CMakeFiles/proj_f.dir/flags.make "Fortran_FLAGS" "-Wno-custom-f-flag"
+
+assert_flag CMakeFiles/proj_c.dir/flags.make "C_FLAGS" "-Wno-common-c-flag"
+assert_flag CMakeFiles/proj_c.dir/flags.make "C_FLAGS" "-Wno-custom-c-flag"
+
+assert_flag CMakeFiles/proj_cxx.dir/flags.make "CXX_FLAGS" "-Wno-common-cxx-flag"
+assert_flag CMakeFiles/proj_cxx.dir/flags.make "CXX_FLAGS" "-Wno-custom-cxx-flag"
--- tests/test_custom_build_type/toolchain.cmake
+++ tests/test_custom_build_type/toolchain.cmake
@@ -3,9 +3,11 @@ set( OpenMP_C_FLAGS                 "-openmp" )
 set( OpenMP_CXX_FLAGS               "-openmp" )
 set( OpenMP_Fortran_FLAGS           "-openmp" )
 
-set( ECBUILD_Fortran_FLAGS          "-common-flag")
+set( ECBUILD_Fortran_FLAGS          "-Wno-common-f-flag")
+set( ECBUILD_C_FLAGS                "-Wno-common-c-flag" )
+set( ECBUILD_CXX_FLAGS              "-Wno-common-cxx-flag" )
 
-set( ECBUILD_Fortran_FLAGS_CUSTOM   "-custom-flag" )
-set( ECBUILD_C_FLAGS_CUSTOM         "-custom-flag" )
-set( ECBUILD_CXX_FLAGS_CUSTOM       "-custom-flag" )
+set( ECBUILD_Fortran_FLAGS_CUSTOM   "-Wno-custom-f-flag" )
+set( ECBUILD_C_FLAGS_CUSTOM         "-Wno-custom-c-flag" )
+set( ECBUILD_CXX_FLAGS_CUSTOM       "-Wno-custom-cxx-flag" )
 
